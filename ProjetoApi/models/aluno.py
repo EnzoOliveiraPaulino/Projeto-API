8c97409f84ca20a29e7064e40be58291f839d791
Christian S Sousa <78213318+ChstianSantos@users.noreply.github.com>
2024-10-19 15:31:29 -0300

    Update aluno.py

--- ProjetoApi/models/aluno.py
+++ ProjetoApi/models/aluno.py
@@ -7,6 +7,6 @@ alunos = [
         "data_nascimento": "2008-05-14",
         "nota_primeiro_semestre": 8.5,
         "nota_segundo_semestre": 7.5,
-        "media_final": (8.5 + 7.5) / 2,
+        "media_final": 8.5,
     }
 ]
