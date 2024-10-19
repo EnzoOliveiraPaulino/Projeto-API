215f0c74f8a89317bf42915bbdb2f86d9429d7ab
Christian S Sousa <78213318+ChstianSantos@users.noreply.github.com>
2024-10-19 15:31:11 -0300

    routes.py

--- ProjetoApi/routes/routes.py
+++ ProjetoApi/routes/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from controllers.professor_controller import get_professores, create_professor, get_professor
 from controllers.turma_controller import get_turmas, create_turma, get_turma
-from controllers.aluno_controller import get_alunos, create_aluno
+from controllers.aluno_controller import get_alunos, create_aluno, update_aluno, delete_aluno
 
 bp = Blueprint('routes', __name__)
 
@@ -18,3 +18,5 @@ bp.route('/turmas/<int:turma_id>', methods=['GET'])(get_turma)
 # Aluno routes
 bp.route('/alunos', methods=['GET'])(get_alunos)
 bp.route('/alunos', methods=['POST'])(create_aluno)
+bp.route('/alunos/<int:aluno_id>', methods=['PUT'])(update_aluno)
+bp.route('/alunos/<int:aluno_id>', methods=['DELETE'])(delete_aluno)
