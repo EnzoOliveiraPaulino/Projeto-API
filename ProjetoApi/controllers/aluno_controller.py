766881e8c9ad1770cb51b9ba3bb3f08fd199ee13
Christian S Sousa <78213318+ChstianSantos@users.noreply.github.com>
2024-10-19 15:32:01 -0300

    aluno_controller.py

--- ProjetoApi/controllers/aluno_controller.py
+++ ProjetoApi/controllers/aluno_controller.py
@@ -18,3 +18,31 @@ def create_aluno():
     }
     alunos.append(aluno)
     return jsonify(aluno), 201
+
+def get_aluno(aluno_id):
+    for aluno in alunos:
+        if aluno['id'] == aluno_id:
+            return jsonify(aluno)
+    return jsonify({'mensagem': 'Aluno não encontrado'}), 404
+
+
+def update_aluno(aluno_id):
+    for aluno in alunos:
+        if aluno['id'] == aluno_id:
+            data = request.json
+            aluno['nome'] = data.get('nome', aluno['nome'])
+            aluno['idade'] = data.get('idade', aluno['idade'])
+            aluno['turma'] = data.get('turma', aluno['turma'])
+            aluno['data_nascimento'] = data.get('data_nascimento', aluno['data_nascimento'])
+            aluno['nota_primeiro_semestre'] = data.get('nota_primeiro_semestre', aluno['nota_primeiro_semestre'])
+            aluno['nota_segundo_semestre'] = data.get('nota_segundo_semestre', aluno['nota_segundo_semestre'])
+            aluno['media_final'] = (aluno['nota_primeiro_semestre'] + aluno['nota_segundo_semestre']) / 2
+            return jsonify(aluno)
+    return jsonify({'mensagem': 'Aluno não encontrado'}), 404
+
+def delete_aluno(aluno_id):
+    for aluno in alunos:
+        if aluno['id'] == aluno_id:
+            alunos.remove(aluno)
+            return jsonify({'mensagem': 'Aluno removido'})
+    return jsonify({'mensagem': 'Aluno não encontrado'}), 404
